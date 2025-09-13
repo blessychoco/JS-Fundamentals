c491d8a185451adf6578220c77caebfa60e9f48d
blessychoco <ezeblessing099@gmail.com>
2025-09-13 10:45:24 +0100

    add 7-multi_c.js for i love c exercise

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const firstArg = process.argv[2];
+const times = parseInt(firstArg, 10);
+
+if (isNaN(times)) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < times; i++) {
+        console.log("C is fun");
+    }
+}
