3f6175f1be66f4d33781f4100383bb60b053e462
blessychoco <ezeblessing099@gmail.com>
2025-09-13 10:16:19 +0100

    add 6-multi_languages_loop.js for loop to language exercise

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+const languages = ["C", "Python", "JavaScript"];
+const descriptions = ["fun", "cool", "amazing"];
+
+for (let i = 0; i < 3; i++) {
+  console.log(languages[i] + " is " + descriptions[i]);
+}
+
+
+
+  
